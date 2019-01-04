f4f885709fed911924e3bc97e99d3135981c1c06
nbloomf <nathan.bloomfield@automattic.com>
2019-01-04 10:18:49 -0600

    Add utility byteN functions

--- src/Data/Char/Decode/Class.hs
+++ src/Data/Char/Decode/Class.hs
@@ -7,12 +7,17 @@ module Data.Char.Decode.Class
   , decodeToString
   , encodeFromString
   , encodeFromStringWithDefault
+  , byte0
+  , byte1
+  , byte2
+  , byte3
   ) where
 
 import           Control.Monad (foldM)
-import           Data.Word (Word8)
-import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as BS
+import           Data.Char (ord)
+import qualified Data.Text as T
+import           Data.Word (Word8)
 
 class ByteEncoding enc where
   decodeByte :: enc -> Word8 -> Char
@@ -62,3 +67,21 @@ encodeFromStringWithDefault
   => enc -> Word8 -> String -> BS.ByteString
 encodeFromStringWithDefault enc def =
   BS.pack . map (encodeWithDefault enc def)
+
+-- Low byte
+lo k = rem k 256
+
+-- High bytes
+hi k = div (k - lo k) 256
+
+byte0 :: Char -> Word8
+byte0 = fromIntegral . lo . ord
+
+byte1 :: Char -> Word8
+byte1 = fromIntegral . lo . hi . ord
+
+byte2 :: Char -> Word8
+byte2 = fromIntegral . lo . hi . hi . ord
+
+byte3 :: Char -> Word8
+byte3 = fromIntegral . lo . hi . hi . hi . ord
